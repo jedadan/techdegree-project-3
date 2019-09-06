38f5397aedc2684c641d7dd67c3cae05aaf3176d
Jed Adan <jedadan@gmail.com>
2019-09-05 22:04:03 -1000

    partial regex validation commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // study guide https://drive.google.com/file/d/1Vw658-9KUiUZ5yHaABvkytC9W2QBYiW_/view
 
+// add focus to the 'name' field on page load
+$('#name').focus();
+
+// hide 'other' text input
+$('#other-title').hide();
+
+// show 'other' text input, when 'other' option is selected
+$('#title').change(function() {
+  if ($(this).val() === 'other') {
+    $('#other-title').show();
+  } else {
+    $('#other-title').hide();
+  }
+});
+
 // functions to show only the colors relevant to the theme
 function showJSPunsColors() {
   $('#color option').remove();
@@ -24,21 +39,6 @@ function showHeartJSColors() {
     '<option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>'
   );
 }
-
-// add focus to the 'name' field on page load
-$('#name').focus();
-
-// hide 'other' text input
-$('#other-title').hide();
-
-// show 'other' text input, when 'other' option is selected
-$('#title').change(function() {
-  if ($(this).val() === 'other') {
-    $('#other-title').show();
-  } else {
-    $('#other-title').hide();
-  }
-});
 
 // hide 'select theme' option
 $("option:contains('Select Theme')").attr('hidden', 'true');
@@ -99,3 +99,151 @@ $('.activities').on('click', function(e) {
 
 // hide 'select payment method' option
 $('#payment [value="select method"]').attr('hidden', 'true');
+
+// credit card option selected by default
+$('#payment option[value="Credit Card"]').attr('selected', 'true');
+
+// determines which payment option is displayed
+$('#paypal').hide();
+$('#bitcoin').hide();
+$('#payment').change(function(e) {
+  const paymentOption = this.target;
+  if ($('#payment').val() === 'PayPal') {
+    $('#credit-card').hide();
+    $('#paypal').show();
+    $('#bitcoin').hide();
+  } else if ($('#payment').val() === 'Bitcoin') {
+    $('#credit-card').hide();
+    $('#paypal').hide();
+    $('#bitcoin').show();
+  } else {
+    $('#credit-card').show();
+    $('#paypal').hide();
+    $('#bitcoin').hide();
+  }
+});
+
+// validation and error for the name input
+const $name = $('#name');
+$($name).after('<span class="nameError">Please enter a valid name</span>');
+$('.nameError').hide();
+
+function validName(name) {
+  if (name !== '') {
+    $('.nameError').hide();
+    return true;
+  } else {
+    $('.nameError').show();
+    return false;
+  }
+}
+
+$name.on('keypress blur', function(e) {
+  validName(e.target.value);
+});
+
+// validation and errors for the email input
+const $email = $('#mail');
+$($email).after('<span class="emailError">Please enter a valid email</span>');
+$($email).after('<span class="emailEmpty">Field cannot remain empty</span>');
+$('.emailError').hide();
+$('.emailEmpty').hide();
+
+function validEmail(email) {
+  let regexEmail = /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
+  if (regexEmail) {
+    $('.emailError').hide();
+    $('.emailEmpty').hide();
+    return true;
+  } else if (email === '') {
+    $('.emailError').hide();
+    $('.emailEmpty').show();
+    return false;
+  } else {
+    $('.emailError').show();
+    $('.emailEmpty').hide();
+    return false;
+  }
+}
+
+$email.on('keypress blur', function(e) {
+  validEmail(e.target.value);
+});
+
+// validation and error for the activity options
+const $activity = $('.activities');
+$($('legend:contains("Register for Activities")')).after(
+  '<span class="activityError">Please select at least one activity</span>'
+);
+$('.activityError').hide();
+
+function validActivity(activity) {
+  if (totalCost !== 0) {
+    $('.activityError').hide();
+    return true;
+  } else {
+    $('.activityError').show();
+    return false;
+  }
+}
+
+$activity.on('click', function(e) {
+  validActivity(e.target.value);
+});
+
+// validation and errors for credit card number input
+const $number = $('#cc-num');
+$($number).after(
+  '<span class="numberError">Credit card number must be between 13 to 19 digits long</span>'
+);
+$($number).after('<span class="numberEmpty">Field cannot remain empty</span>');
+$('.numberError').hide();
+$('.numberEmpty').hide();
+
+function validNumber(cardnumber) {
+  let regexNumber = /^\d{13,19}$/.test(cardnumber);
+  if (regexNumber) {
+    $('.numberError').hide();
+    $('.numberEmpty').hide();
+    return true;
+  } else if (cardnumber === '') {
+    $('.numberError').hide();
+    $('.numberEmpty').show();
+    return false;
+  } else {
+    $('.numberError').show();
+    $('.numberEmpty').hide();
+    return false;
+  }
+}
+
+$number.on('keypress blur', function(e) {
+  validNumber(e.target.value);
+});
+
+// validation and errors for zip code input
+const $zip = $('#zip');
+$($zip).after('<span class="zipError">Zip code must be 5 digits long</span>');
+$($zip).after('<span class="zipEmpty">Field cannot remain empty</span>');
+$('.zipError').hide();
+$('.zipEmpty').hide();
+
+function validZip(zip) {
+  let regexZip = /^\d{5}$/.test(zip);
+  if (regexZip) {
+    $('.zipError').hide();
+    $('.zipEmpty').hide();
+    return true;
+  } else if (zip === '') {
+    $('.zipError').hide();
+    $('.zipEmpty').show();
+    return false;
+  } else {
+    $('.zipError').show();
+    $('.zipEmpty').hide();
+  }
+}
+
+$zip.on('keypress blur', function(e) {
+  validZip(e.target.value);
+});
